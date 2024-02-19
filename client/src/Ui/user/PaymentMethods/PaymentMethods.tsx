49ae5036433a2a0c7ffb6c417d7971c3ac89320f
Ashish_Devashi <ashish.devashi@reputeinfo.com>
2024-02-19 22:48:22 +0530

    Account,History,Payment added in User

--- client/src/Ui/user/PaymentMethods/PaymentMethods.tsx
+++ client/src/Ui/user/PaymentMethods/PaymentMethods.tsx
@@ -1,9 +1,21 @@
+import { Card, CardBody } from '@nextui-org/react'
 import React from 'react'
+import { AddCard, CardWithData } from '../component'
 
 function PaymentMethods() {
   return (
-    <div>
-         hiihihihi
+    <div className="mt-6">
+      <h2 className="text-3xl font-bold text-black mb-3">Payment methods</h2>
+      <Card className="rounded-sm shadow-md">
+        <CardBody>
+          <div className='flex'>
+            <CardWithData />
+            <div className='ml-5'>
+              <AddCard />
+            </div>
+          </div>
+        </CardBody>
+      </Card>
     </div>
   )
 }
--- client/src/Ui/user/User.tsx
+++ client/src/Ui/user/User.tsx
@@ -11,15 +11,15 @@ export default function User() {
     const TabClassName = {
         tabList: "flex justify-between w-full relative rounded-none p-0 bg-white shadow-md ",
         cursor: "w-full bg-[#8DD3BB] h-1",
-        tab: "h-12 font-semibold",
+        tab: "h-12 font-semibold border-r-2",
     }
 
     return (
         <div className="flex w-full flex-col  my-5 px-20" >
             <Tabs classNames={TabClassName} aria-label="Options" variant='underlined' >
-                <Tab key="Account" title="Account" className='border-r-2' ><Account/></Tab>
-                <Tab key="History" title="History"  className='border-r-2' ><History/></Tab>
-                <Tab key="Payment methods" title="Payment methods" ><PaymentMethods/></Tab>
+                <Tab key="Account" title="Account"  ><Account /></Tab>
+                <Tab key="History" title="History"   ><History /></Tab>
+                <Tab key="Payment methods" title="Payment methods" ><PaymentMethods /></Tab>
             </Tabs>
         </div>
     )
