1afde1a5e68c3ec9d109cc642e37f137d2a488d9
Ashish_Devashi <ashish.devashi@reputeinfo.com>
2024-03-08 16:17:02 +0530

    Ui folder deleted

--- client/src/Ui/NotFound/NotFound.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function NotFound() {
-    return (
-        <div className='grid place-items-center border'>
-            <h2>Not Found Pagee</h2>
-        </div>
-    )
-}
